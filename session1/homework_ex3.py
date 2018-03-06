013bc7a8621a8a859c6d263e18b409ce06ffc969
Mirela Iclodean <miclodean@twitter.com>
2018-03-06 21:24:54 +0000

    [cfg] Session 1 - homework exercise 3

---
+++ session1/homework_ex3.py
@@ -0,0 +1,9 @@
+# Type the following text into a single file named ex1.py. Python works best with files ending in .py.
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
