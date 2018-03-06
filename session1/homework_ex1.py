3966b6db2123f1eb527689554ddbfca902fd51ae
Mirela Iclodean <miclodean@twitter.com>
2018-03-06 20:36:33 +0000

    [cfg] Session 1 - homework exercise 1

---
+++ session1/homework_ex1.py
@@ -0,0 +1,11 @@
+# Add code your Python file to print the answers to these expressions in
+# your command line:
+
+# Result should be 3
+print 10 / 3
+# Result should be 3.33333333333
+print 10.0 / 3.0
+# Result should be 1
+print 10 % 3
+# Result should be 1.0
+print 10.0 % 3.0 # 1.0
