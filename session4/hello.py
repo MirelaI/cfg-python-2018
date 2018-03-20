7e58b8e1e1978bd020de9fa9f7e92d3340842a8d
Mirela Iclodean <mirela.iclodean@gmail.com>
2018-03-20 23:55:15 +0000

    [Session 4] Add more routes

--- session4/hello.py
+++ session4/hello.py
@@ -1,4 +1,7 @@
+ # -*- coding: utf-8 -*-
+
 from flask import Flask
+import random
 
 app = Flask("MyApp")
 
@@ -9,5 +12,10 @@ def hello_world():
 @app.route('/hello/<username>')
 def hello_username(username):
     return "Hello %s" % username
+
+@app.route('/brownies/<username>')
+def brownies_for_user(username):
+    brownies_allowed = random.randint(1,10)
+    return '%s is allowed to %d brownies today!' % (username, brownies_allowed)
 
 app.run(debug=True)
