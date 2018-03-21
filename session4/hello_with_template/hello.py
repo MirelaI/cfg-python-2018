8377271341b33083b6ae9403c05cd63ec4f44805
Mirela Iclodean <mirela.iclodean@gmail.com>
2018-03-21 10:30:48 +0000

    [Session 4] Webapp hello world using a template

---
+++ session4/hello_with_template/hello.py
@@ -0,0 +1,15 @@
+ # -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request
+
+app = Flask("MyApp")
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
+app.run(debug=True)
